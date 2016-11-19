3afcfbeec607208e5a69814bbf8f592344ec632d
sherryzw <zhaowen.tju@gmail.com>
2016-11-19 14:37:57 -0500

    Schema update

--- models/users.js
+++ models/users.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema(
 {
-	username: {
+	Username: {
 		type: String, required: true, unique: true
 	},
 	givenname: {
@@ -20,27 +20,15 @@ var userSchema = new Schema(
 	email: {
 		type: String, required: true, unique: true
 	},
-<<<<<<< HEAD:model-schema/users.js
 	hashed: {
 		type:String, required: true
 	},
 	salt:{
 		type:String, required: true
-=======
-	gender: {
-		type: String, required:true
-	},
-	hashedPassword: {
-		hashed:String, salt:String
->>>>>>> origin/master:models/users.js
-	},
-	profilePicture:{//path to the profile picture
-		type: String, default:"defaultPicture" //some default profile picture 
 	},
 	role: {
 		type: String, required:true, //the role can be "mentee" or "mentor"
 	},
-<<<<<<< HEAD:model-schema/users.js
 	specialty:[String],
 	profilePicture:{//path to the profile picture
 		type: String, default:"defaultPicture" //some default profile picture 
@@ -48,8 +36,6 @@ var userSchema = new Schema(
 	gender: {
 		type: String, required:true
 	},
-=======
->>>>>>> origin/master:models/users.js
 	birthday: {
 		type: Date, required: true
 	},
@@ -66,9 +52,6 @@ var userSchema = new Schema(
 }
 );
 
-<<<<<<< HEAD:model-schema/users.js
 mongoose.connect('mongodb://localhost/mentorDB');
-=======
->>>>>>> origin/master:models/users.js
 
 module.exports = mongoose.model('Users', userSchema);
