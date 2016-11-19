f2a4bcee682f366bd180822c03ccded4b1078978
Tallis Jeng <alog1024@gmail.com>
2016-11-18 20:45:35 -0500

    add schema for parts other than users

---
+++ model-schema/data.js
@@ -0,0 +1,90 @@
+var mongoose = require('mongoose');
+
+// Doc for Mongoose Schemas: http://mongoosejs.com/docs/guide
+var Schema = mongoose.Schema;
+
+/**
+ * Note that the database was loaded with data from a JSON file into a
+ * collection called gillers.
+ */
+var chat = new Schema({
+    speaker1_id: {
+        type: String,
+        required: true
+    },
+    speaker2_id: {
+        type: String,
+        requried: true
+    },
+    msgs: [
+        sender_num: {
+            type: String
+        },
+        text: {
+            type: String
+        },
+        file: {
+            type: Object
+        },
+        time: {
+            time: String,
+            required: true
+        }
+        ]
+}, {
+    collection: 'chats'
+});
+
+var admin = new Schema({
+    user_name”: {
+        type: String,
+        required: true
+    },
+    “Password”: {“
+        hashed”: {
+            type: String,
+            required: true
+        },
+        “salt”: {
+            type: String,
+            required: true
+        }
+    }
+
+}, {
+    collection: 'admins'
+});
+
+var mentee = new Schema({
+    user_id: {
+        type: String,
+        required: true
+    }
+}, {
+    collection: 'mentees'
+});
+
+var mentor = new Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
+    mentee_list[
+        mentee_id: {
+            type: String,
+            required: true
+        }
+        ],
+} {
+    collection: 'mentors'
+});
+
+
+
+
+
+var Chat = mongoose.model('Chat', chat);
+var Admin = mongoose.model('Admin', admin);
+// Doc for Mongoose Models: http://mongoosejs.com/docs/models
+module.exports = Chat;
+module.exports = Admin;
