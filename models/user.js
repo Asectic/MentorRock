a3444ba36eabe51c00d3d1299e071f28e19bbefd
sherryzw <zhaowen.tju@gmail.com>
2016-11-22 13:37:18 -0500

    remove twitter & google login

--- models/user.js
+++ models/user.js
@@ -28,18 +28,6 @@ var userSchema = mongoose.Schema({
         id           : { type: String, },
         token        : { type: String, },
         email        : { type: String, }
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
     }
 
 });
